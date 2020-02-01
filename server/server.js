5904a2a09703e0e472999acb39764971acacd07d
Anand <anand.patil.ap79@gmail.com>
2020-02-01 13:36:37 +0530

    .env fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const dotnev = require('dotenv');
 const app = require('./app.js');
 
-dotnev.config({ path: './config.env' });
+dotnev.config({ path: './.env' });
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
